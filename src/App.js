fda1103e5e6fcd0b22e75ab584913650554a1aa0
shevon2000 <120797907+shevon2000@users.noreply.github.com>
2024-06-05 07:28:37 +0530

    about to go over props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+
+  console.log('we are about to list employees');
+  const showEmployees = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log('inside the return')}
+      {showEmployees ?
+        <>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+      :
+        <p>You cannot see the employees</p>
+      }
     </div>
   );
 }
 
 export default App;
+
+/* 
+
+  ternary operator,
+    {condition ? if ture : if false}
+  -------------------------------------------------------------------------------------------------------------
+
+  react components always return a single element (html)
+    so if we have multiple to return, we can put them into a single div or single html element.
+  -------------------------------------------------------------------------------------------------------------
+
+  component nesting
+    inside App() component we have Employee components, and they donnot need to know about the parent component
+
+  */
